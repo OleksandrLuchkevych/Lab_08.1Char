9e12ff2ea8cc11ca605e70f53b72cc2d6d010f8f
oleksandr.luchkevych.ri.2022@lpnu.ua <oleksandr.luchkevych.ri.2022@lpnu.ua>
2022-12-02 11:51:13 +0200

    Add project files.

---
+++ Lab_08.1Char/Lab_08.1Char.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+int Count(char* s)
+{
+	int k = 0,
+		pos = 0;
+	char* t;
+	while (t = strchr(s + pos, '.'))
+	{
+		pos = t - s + 1;
+		k++;
+		if (k == 3)
+			return pos;
+	}
+	return 0;
+}
+char* Change(char* s) // s = вказівник на початок рядка
+{
+	int len = int(strlen(s) * 1. / 1 + 1 );
+	char* t = new char[len];
+	
+	strcpy_s(t, len, s);
+	
+	for (int i = 3; t[i] != '\0'; i += 4)
+	{	
+		t[i] = '.';
+	}	
+	
+	return t;
+}
+
+int main()
+{
+	char str[101];
+	cout << "Enter string:" << endl;
+	cin.getline(str, 100);
+	cout << "Index of 3rd dot: " << Count(str)<< endl;
+	char* dest = new char[151];
+
+	dest = Change(str);
+	cout << "Initial string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_08.1Char/Lab_08.1Char.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[]{ "hi...kl" };
+			int t = Count(str);
+			Assert::AreEqual(t, 5);
+		}
+	};
+}
